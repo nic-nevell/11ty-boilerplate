4a10380f855cc656b2f6804c470741f2c6bbd024
nic-nevell <nicnevell@gmail.com>
2023-01-20 16:35:00 +0000

    fixed admin

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/main.css')
   eleventyConfig.addPassthroughCopy('./src/images/*.svg')
   eleventyConfig.addPassthroughCopy('./src/images/*.jpg')
+  eleventyConfig.addPassthroughCopy('./src/admin/')
 
   eleventyConfig.addFilter('postDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
