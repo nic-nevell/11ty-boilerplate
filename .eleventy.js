8e9b1dc5a70d6821df32ea40a6c60796b5d1cad4
nic-nevell <nicnevell@gmail.com>
2023-01-20 15:37:20 +0000

    first comit

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+const { DateTime } = require('luxon')
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('./src/main.css')
+  eleventyConfig.addPassthroughCopy('./src/images/*.svg')
+  eleventyConfig.addPassthroughCopy('./src/images/*.jpg')
+
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+  })
+
+  return {
+    dir: {
+      input: './src',
+      output: './dist/',
+    },
+  }
+}
---
+++ src/posts/post-001.md
@@ -0,0 +1,13 @@
+---
+tags: post
+permalink: false
+
+title: My First Post
+author: Nic Nevell
+date: 2023-01-20
+
+image: /images/conny-schneider.jpg
+imageALt: image description
+url: https://wwww.weberdy.com/
+content: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate facilis doloribus omnis, facere inventore eos rem dicta fuga? Provident quisquam incidunt autem accusamus nobis sit fugit pariatur excepturi non distinctio.
+---
