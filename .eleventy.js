684070998420b8b7784e58eef4fd1e9ae9446961
nic-nevell <nicnevell@gmail.com>
2023-02-07 12:57:11 +0000

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 const { DateTime } = require('luxon')
+const  htmlMin  = require('html-minifier')
+const  postHtml  = require('posthtml')
+const  uglify  = require('posthtml-minify-classnames')
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/admin/')
@@ -12,6 +15,21 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter('cleanImageExtension', (fileName) => {
     return fileName.substring(0, fileName.lastIndexOf('.')) || fileName
   })
+
+  //   eleventyConfig.addTransform("htmlMin", async function(content, outputPath) {
+  //     const { html } = await postHtml().use(uglify()).process(content)
+  //   if( outputPath.endsWith(".html") ) {
+  //     let minified = htmlMin.minify(content, {
+  //       useShortDoctype: true,
+  //       removeComments: true,
+  //       collapseWhitespace: true
+  //     });
+  //     return minified;
+  //   }
+
+  //   return content;
+  // });
+
 
   return {
     dir: {
